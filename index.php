7d451b081e90d7dbd0a07eda0a58bd748fd5f2bc
vcella <cella.vladimir@gmil.com>
2019-12-22 20:05:26 -0300

    araquivo modificado

--- index.php
+++ index.php
@@ -1,3 +1,14 @@
+<html lang="en">
+<head>
+   <meta charset="UTF-8">
+   <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   <meta http-equiv="X-UA-Compatible" content="ie=edge">
+   <title>Exemplo - Teste de deploy automático</title>
+</head>
+<body>
+<h1>Dados do servidor</h1>
 <?php
    phpinfo();
 ?>
+</body>
+</html>
